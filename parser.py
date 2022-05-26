fd7077d00328d3d1e8833d0f07d9dd59c0101057
vilniemi <vilniemi@users.noreply.github.com>
2022-05-26 15:06:30 +0300

    save tracks to dictionary

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ lines = f.readlines()
 def get_note(str):
     exit
 tracks = []
+tracks_dict = {}
 tempo = 0
 for line in lines:
     if (line[0] == '#'):
@@ -21,20 +22,37 @@ for line in lines:
         tracks = data[0].split(',')
     if (re.search("(\d*):", data[0])):
         tracknum = data[0].split(':')
-        print (tracknum[0])
+        tracknum = tracknum[0]
         data.pop(0)
         #print (data)
+        track = []
         for elem in data:
             elem = elem.split('/')
             note = elem[0]
+            tune = {}
             if (len(elem) == 1):
                 seconds = "0"
             else:
                 seconds = elem[1]
-            print ("note: "+note+" seconds: "+seconds)
+            tune['seconds'] = seconds
+            tune['tone'] = note
+            #print ("note: "+note+" seconds: "+seconds)
+            #print (tune)
+            track.append(tune)
+        
+        tracks_dict['tracknum'] = tracknum
+        if (int(tracknum)-1 < len(tracks)):
+            tracks_dict['wave'] = tracks[int(tracknum)-1]
+        else:
+            print ('error on '+tracknum+' track not exists')
+        #print (tracks[0])
+        tracks_dict['track'] = track
+    
+
    # if data[0] == 
     
     #print (data)
     #print(line, end="")
-print (tempo)
-print (tracks)
+print ("tempo: "+tempo)
+#print (tracks)
+print (tracks_dict)
